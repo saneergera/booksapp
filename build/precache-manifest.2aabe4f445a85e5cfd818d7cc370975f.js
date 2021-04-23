82196c7df9740701a66cbd9cd9bfd5336e42d555
saneer gera <saneergera@saneers-MacBook-Air.local>
2021-04-23 11:22:41 -0400

    DEPLOY - the app

---
+++ build/precache-manifest.2aabe4f445a85e5cfd818d7cc370975f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ecf331fde458babace2c70dc1ed77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6db7479f3260a2f2c21",
+    "url": "/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "35269654bc29c55f70ba",
+    "url": "/static/css/main.3d4b2425.chunk.css"
+  },
+  {
+    "revision": "b6db7479f3260a2f2c21",
+    "url": "/static/js/2.c8e8415e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c8e8415e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35269654bc29c55f70ba",
+    "url": "/static/js/main.5ef3ff7a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
